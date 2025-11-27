a5da93ee2bb377383f997d4237837e326264ff34
musa-qadri <musaqazim1@gmail.com>
2025-11-27 12:36:02 +0500

    component of channel

---
+++ app/[locale]/dashboard/Channals/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { DashboardContent } from "../components/dashboard-content";
+
+export default function DashboardPage() {
+  return (
+    <div className=" bg-white">
+      <DashboardContent
+        sample={"Daily Call Reports"}
+        descrip={"Welcome to Ceutro, It's looking like a slow day"}
+        table={"Calls Report"}
+        btnTrue={true}
+        campHeading={"All Channals"}
+        filterT={true}
+        hideMetrics={true}
+        channalD={true}
+        hideHeader={false}
+      />
+    </div>
+  );
+}
