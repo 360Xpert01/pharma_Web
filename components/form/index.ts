4e513ee37079632a5b7cb5ca1c15ffbc60e970d9
zahidrahimoon <zahidrahimoon22@gmail.com>
2026-02-06 20:42:24 +0500

    feat: multi select done

--- components/form/index.ts
+++ components/form/index.ts
@@ -2,6 +2,7 @@
 export { default as ProfileImageUpload } from "./ProfileImageUpload";
 export { default as FormInput } from "./FormInput";
 export { default as FormSelect } from "./FormSelect";
+export { default as FormMultiSelect } from "./FormMultiSelect";
 export { default as StatusToggle } from "./StatusToggle";
 export { default as ProductSearch } from "./ProductSearch";
 export { default as MemberSearch } from "./MemberSearch";
